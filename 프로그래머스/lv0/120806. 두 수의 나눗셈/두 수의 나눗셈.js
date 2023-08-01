ba7d24c1ec0308fde290fbebe92c147be36e11d7
Young2un <132687752+Young2un@users.noreply.github.com>
2023-08-01 22:39:12 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -0,0 +1,27 @@
+// const solution = (num1, num2)=> parseInt((num1/num2)*1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function solution(num1, num2) {
+   
+    const answer =  Math.trunc(num1/num2*1000);
+    return answer;
+}
+
+
+
+
+
+
+
