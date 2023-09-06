ffd882eca6ce3cdfa4c1f07b1c31162e870ee5c7
Young2un <132687752+Young2un@users.noreply.github.com>
2023-09-06 11:34:22 +0900

    [level 0] Title: 두 수의 차, Time: 2.01 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.js
@@ -0,0 +1,11 @@
+// const solution = (num1, num2)=> num1-num2;
+
+
+
+function solution(num1, num2) {
+    // var answer = '';
+    // return answer;
+    console.log()
+  return num1 - num2
+    
+}
