eaece58e353c4d0a9be0879aaf5a33573e268a31
Young2un <132687752+Young2un@users.noreply.github.com>
2023-09-13 16:28:47 +0900

    [unrated] Title: 배열 만들기 4, Time: 7.23 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -1,25 +1,25 @@
 function solution(arr) {
     var stk = [];
     
-    // for(let i = 0; i < arr.length; i++){
-    //     if ((stk.length === 0) ||(stk[stk.length-1] < arr[i])){
-    //         stk = [...stk, arr[i]]
-    //     } else {
-    //        stk.length -= 1 
-    //         i--
-    //     } 
-    // }
-    //     return stk
-    
-    let i = 0;
-    while (i < arr.length){
-         if ((stk.length === 0) ||(stk[stk.length-1] < arr[i])){
+    for(let i = 0; i < arr.length; ){
+        if ((stk.length === 0) ||(stk[stk.length-1] < arr[i])){
             stk = [...stk, arr[i]]
-             i++
+            i++
         } else {
            stk.length -= 1 
         } 
     }
-    return stk
+        return stk
+    
+    // let i = 0;
+    // while (i < arr.length){
+    //      if ((stk.length === 0) ||(stk[stk.length-1] < arr[i])){
+    //         stk = [...stk, arr[i]]
+    //          i++
+    //     } else {
+    //        stk.length -= 1 
+    //     } 
+    // }
+    // return stk
     
 }
