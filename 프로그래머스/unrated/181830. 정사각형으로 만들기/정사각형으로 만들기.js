846927443475eea096c49ab0dd3a8a972eff3a71
Young2un <132687752+Young2un@users.noreply.github.com>
2023-09-18 16:05:07 +0900

    [unrated] Title: 정사각형으로 만들기, Time: 0.08 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.js
+++ 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -1,17 +1,28 @@
+// function solution(arr) {
+//     let zero = []
+//     const add = arr[0].length - arr.length
+//     if (arr.length < arr[0].length){
+//         for(let i= 0; i < arr[0].length ; i++){
+//             zero = [...zero,0]
+//         }
+//         arr.length = arr[0].length
+//         arr.fill(zero,-add)
+//     }else if(arr.length > arr[0].length) {
+//           for(let i= 0; i < arr.length ; i++){
+//               arr[i].length = arr.length
+//               arr[i].fill(0,add)
+//           }
+//     }
+//     return arr
+// }
+
 function solution(arr) {
-    let zero = []
-    const add = arr[0].length - arr.length
-    if (arr.length < arr[0].length){
-        for(let i= 0; i < arr[0].length ; i++){
-            zero = [...zero,0]
-        }
-        arr.length = arr[0].length
-        arr.fill(zero,-add)
-    }else if(arr.length > arr[0].length) {
-          for(let i= 0; i < arr.length ; i++){
-              arr[i].length = arr.length
-              arr[i].fill(0,add)
-          }
+    const needZero = Math.abs(arr.length - arr[0].length);
+    if(arr.length > arr[0].length){
+        return arr.map(v => v = [...v, ...Array(needZero).fill(0)])
+    }else if(arr.length < arr[0].length){
+        const zeroArr = Array(arr[0].length).fill(0);
+        return [...arr, ...Array(needZero).fill(zeroArr)]
     }
-    return arr
+    return arr;
 }
