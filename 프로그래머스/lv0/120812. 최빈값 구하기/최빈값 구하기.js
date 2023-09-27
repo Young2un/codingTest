e2d6a6b3d136c160f99f7b6376f0de543a4a4b12
Young2un <132687752+Young2un@users.noreply.github.com>
2023-09-27 19:58:10 +0900

    [level 0] Title: 최빈값 구하기, Time: 2.37 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -0,0 +1,11 @@
+function solution(array) {
+   const arr = array.reduce((a,c)=>{
+        a[c] = (a[c] || 0) + 1;
+        return a
+    },{})
+   const arrSort = Object.entries(arr).sort((a, b) => b[1] - a[1])
+   console.log(arrSort[0][1])
+    
+    // console.log(arrSort.map(v=>v[1]))
+   return arrSort.length >1 && arrSort[0][1] === arrSort[1][1] ? -1 : +arrSort[0][0]
+}
