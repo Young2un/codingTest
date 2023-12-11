0dbec6dff835612e8977a46699d896df48c09abe
Young2un <132687752+Young2un@users.noreply.github.com>
2023-12-12 00:29:49 +0900

    [level unrated] Title: 직사각형 넓이 구하기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/unrated/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,13 +1,11 @@
 function solution(dots) {
-    const maxMin = (arr) => {
-   const a = Math.max(...arr)
-   const b = Math.min(...arr)
-   return Math.abs(a-b)
-}
     const map = dots.map(v=>v[0])
     const map1 = dots.map(v=>v[1])
-    
+    const maxMin = (arr) => {
+        const a = Math.max(...arr)
+        const b = Math.min(...arr)
+        return Math.abs(a-b)
+    }
     return maxMin(map)*maxMin(map1)
-    
 }
 
