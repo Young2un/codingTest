632faff9102513da962c56979ac673066f260768
Young2un <132687752+Young2un@users.noreply.github.com>
2023-12-26 22:58:05 +0900

    [Bronze III] Title: 윷놀이, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.js
+++ 백준/Bronze/2490. 윷놀이/윷놀이.js
@@ -4,7 +4,7 @@ const input = require('fs')
   .split('\n')
   .map(v => v.split(' '));
 
-const solution = input => {
+function solution(input) {
   const yut = {
     4: 'E',
     0: 'D',
@@ -14,9 +14,7 @@ const solution = input => {
   };
 
   const arr = input.map(v => v.reduce((a, b) => a + +b, 0));
-  return arr.map(v => yut[v]);
-};
-
-for (let i = 0; i < solution(input).length; i++) {
-  console.log(solution(input)[i]);
+  return arr.map(v => yut[v]).join('\n');
 }
+
+console.log(solution(input));
