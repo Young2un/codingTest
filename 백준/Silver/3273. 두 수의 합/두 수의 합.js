9a3cc91dd32234a6a01c1ce10a0a38fcf9165098
Young2un <132687752+Young2un@users.noreply.github.com>
2024-01-05 04:23:44 +0900

    [Silver III] Title: 두 수의 합, Time: 244 ms, Memory: 29068 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.js
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.js
@@ -3,12 +3,8 @@ const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('
 const [, arr, x] = input;
 
 const sequence = arr.split(' ').map(Number);
-
 const sub = sequence.filter(v => x - v !== v).map(r => x - r);
-
 const filterArr = new Set(sub);
-
 const filter = sequence.filter(v => filterArr.has(v));
 
-console.log(filter.length/2);
-
+console.log(filter.length / 2);
