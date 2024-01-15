8d8b8a358beb9ddce8a194e048963a2e26c94a02
Young2un <132687752+Young2un@users.noreply.github.com>
2024-01-16 02:56:51 +0900

    [Silver IV] Title: 큐 2, Time: 2056 ms, Memory: 307768 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let head = 0;
+let tail = 0;
+let queue = Array(2000000); // 초기 큐 크기 설정
+
+const operations = {
+  push: (x) => {
+    queue[tail++] = x;
+  },
+  pop: () => (head === tail ? -1 : queue[head++]),
+  size: () => tail - head,
+  empty: () => (head === tail ? 1 : 0),
+  front: () => (head === tail ? -1 : queue[head]),
+  back: () => (head === tail ? -1 : queue[tail - 1]),
+};
+
+const result = [];
+
+for (let i = 1; i < input.length; i++) {
+  const [cmd, value] = input[i].split(' ');
+  const output = operations[cmd](Number(value));
+  if (output !== undefined) {
+    result.push(output);
+  }
+}
+
+console.log(result.join('\n'));
