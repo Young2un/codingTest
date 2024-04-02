2639400f2db36c95aedfcbc9d68525f9f9ccc1f6
Young2un <132687752+Young2un@users.noreply.github.com>
2024-04-03 02:41:25 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,3 +1,6 @@
-function solution(my_string) {
-    return [...my_string].map(v=>+v).join('').split('NaN').reduce((a,c)=>a + +c,0)
-}
+const solution = (my_string) =>
+  [...my_string]
+    .map((v) => +v)
+    .join("")
+    .split("NaN")
+    .reduce((a, c) => a + +c, 0);
