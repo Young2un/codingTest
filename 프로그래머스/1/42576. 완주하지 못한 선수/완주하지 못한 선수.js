986741553936d40f7b1399c986f86294e8c39cd9
Young2un <132687752+Young2un@users.noreply.github.com>
2024-05-28 10:21:09 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.61 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,23 +1,37 @@
-function solution(participant, completion) {
-   // return participant.filter(v=> !completion.includes(v)).join('')
+// function solution(participant, completion) {
+//    // return participant.filter(v=> !completion.includes(v)).join('')
     
-  const racer = participant.reduce((a,c)=>{
-        a[c]=(a[c]||0)+1
-        return a
-    },{})
+//   const racer = participant.reduce((a,c)=>{
+//         a[c]=(a[c]||0)+1
+//         return a
+//     },{})
   
-const complete=  completion.reduce((a,c)=>{
-        a[c]=(a[c]||0)-1
-        return a
-    },racer)
+// const complete=  completion.reduce((a,c)=>{
+//         a[c]=(a[c]||0)-1
+//         return a
+//     },racer)
 
-const arr = Object.entries(complete)
+// const arr = Object.entries(complete)
 
-const filterArr = arr.filter((v)=>v[1])
+// const filterArr = arr.filter((v)=>v[1])
+// console.log(complete)
+// // const ppp= ['aa','bb']
+// // console.log( ppp.join(''))
 
-// const ppp= ['aa','bb']
-// console.log( ppp.join(''))
+// return filterArr.map(v=>v[0]).join('')
 
-return filterArr.map(v=>v[0]).join('')
+// }
 
+function solution(participant, completion) {
+    const countMap = participant.reduce((map, person) => {
+        map.set(person, (map.get(person) || 0) + 1);
+        return map;
+    }, new Map());
+    
+    completion.reduce((map, person) => {
+        map.set(person, map.get(person) - 1);
+        return map;
+    }, countMap);
+    
+    return [...countMap.entries()].find(([key, value]) => value > 0)[0];
 }
