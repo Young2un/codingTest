8630c1a87f11ce5cb8b946262a6a1d7f073a5389
Young2un <132687752+Young2un@users.noreply.github.com>
2025-01-05 13:41:41 +0900

    [level 2] Title: 주식가격, Time: 6.28 ms, Memory: 42.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,7 +1,6 @@
 function solution(prices) {
-    
-  
     const answer=[];
+    
     for (let i = 0; i<prices.length; i++){
           let time = 0;
         for(let j = i+1; j<prices.length; j++){
