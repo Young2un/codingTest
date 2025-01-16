b9b48f04c965a4b0334f21ee25322ebf5ea0fc1c
Young2un <132687752+Young2un@users.noreply.github.com>
2025-01-16 15:39:52 +0900

    [level 2] Title: 카펫, Time: 0.52 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,15 +1,8 @@
 function solution(brown, yellow) {
     const total = brown+yellow 
     
-    // 전체 갯수에서 약수를 구한다.
-    // t = w * h 
-    // y = (w-2) * (h-2)
-    // b = t - y
-
-    // b = t - (w-2) * (h-2)
-  const divisors = Array.from({ length: Math.sqrt(total) }, (_, i) => i + 1)
+    return Array.from({ length: Math.sqrt(total) }, (_, i) => i + 1)
     .filter((v) => total % v === 0) 
-    .map((v) => [total / v, v]);
-    
-    return divisors.filter(([w,h])=> yellow === ((w-2)*(h-2))).flat()
+    .map((v) => [total / v, v])
+    .find(([w, h]) => yellow === (w - 2) * (h - 2))
 }
