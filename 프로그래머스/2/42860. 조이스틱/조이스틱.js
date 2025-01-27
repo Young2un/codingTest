16dbbd5e6acbeefb23fa81d12a594d444f993ff2
Young2un <132687752+Young2un@users.noreply.github.com>
2025-01-27 10:58:33 +0900

    [level 2] Title: 조이스틱, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.js
@@ -0,0 +1,48 @@
+function solution(name) {
+  // const alphabet = ['A', 'B', 'C', 'D', 'E', 'F','G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 'P', 'Q', 'R','S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+ 
+const alphabetObject = {
+  A: 0,
+  B: 1,
+  C: 2,
+  D: 3,
+  E: 4,
+  F: 5,
+  G: 6,
+  H: 7,
+  I: 8,
+  J: 9,
+  K: 10,
+  L: 11,
+  M: 12,
+  N: 13,
+  O: 12,
+  P: 11,
+  Q: 10,
+  R: 9,
+  S: 8,
+  T: 7,
+  U: 6,
+  V: 5,
+  W: 4,
+  X: 3,
+  Y: 2,
+  Z: 1,
+};
+
+  const alphabetSum = [...name].map(v=>alphabetObject[v]).reduce((a,c)=>a+c,0)
+  let moveCount = name.length - 1; 
+  
+    // "ABAAAAAAAAABB"
+    for (let i = 0; i < name.length; i++) {
+        let next = i + 1;
+       
+        while (next < name.length && name[next] === 'A') {
+            next++;
+        }
+        moveCount = Math.min(moveCount,i + name.length - next + Math.min(i, name.length - next)
+    );
+  }
+
+  return alphabetSum + moveCount;
+}
