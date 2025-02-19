d40144506526b25e496a425b31a617dcbcdf2a8b
Young2un <132687752+Young2un@users.noreply.github.com>
2025-02-19 11:23:23 +0900

    [level 2] Title: 타겟 넘버, Time: 28.42 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,29 +1,19 @@
 function solution(numbers, target) {
-  // numbers 배열의 합을 미리 계산하여 memoization에 활용
-  const totalSum = numbers.reduce((acc, num) => acc + num, 0);
-  const memo = new Map();
+  // number를 2중배열로 만든다
+    const numArr = numbers.map(v=> [v,v*-1])
+    let count = 0;
+    // 재귀돌면서 더한다
+    function recur(index, sum) {
+        if (index === numArr.length) {
+            // 더한게 타겟이랑 같으면 카운트업
+            if (sum === target) count++; 
+            return;
+        }
 
-  function dfs(index, sum) {
-    const memoKey = `${index}-${sum}`;
-    if (memo.has(memoKey)) return memo.get(memoKey);
-
-    // 현재 요소까지의 합이 타겟과의 차이가 전체 합보다 크다면 탐색할 필요 없음
-    if (Math.abs(target - sum) > totalSum) return 0;
-      
-    // 기저 조건
-    if (index === numbers.length) {
-      if (sum === target) return 1;
-      return 0;
+        recur(index + 1, sum + numArr[index][0]); 
+        recur(index + 1, sum + numArr[index][1]);
     }
-     // 현재 요소를 더하거나 빼는 두 가지 경우의 수에 대해 재귀적으로 계산
-    const ways =
-      dfs(index + 1, sum + numbers[index]) +
-      dfs(index + 1, sum - numbers[index]);
 
-    memo.set(memoKey, ways);
-
-    return ways;
-  }
-
-  return dfs(0, 0);
+    recur(0, 0);
+    return count;
 }
